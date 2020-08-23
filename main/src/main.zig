5a550db4f826bf0aa9658b0d9c5f71d0fe22e853
fourst4r <oxygenmagnet@hotmail.com>
2020-08-24 06:48:30 +1200

    pogress

--- main/src/main.zig
+++ main/src/main.zig
@@ -4,9 +4,15 @@ const print = std.debug.print;
 const SwfFile = @import("zwf").SwfFile;
 
 pub fn main() !void {
-    const allocator = std.testing.allocator;
-    const data = try fs.cwd().readFileAlloc(allocator, "./assets/platform-racing-2-v159.swf", 0xffffff);
-    const swf = SwfFile.read(data);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    var file = try fs.cwd().openFile("./assets/platform-racing-2-v159.swf", .{});
+    defer file.close();
+    // const data = try fs.cwd().readFileAlloc(allocator, "./assets/platform-racing-2-v159.swf", 0xffffff);
+    // defer allocator.free(data);
+
+    const swf = try SwfFile.init(file.reader());
     print("{}\n", .{swf.header});
-    
 }
--- src/swf_file.zig
+++ src/swf_file.zig
@@ -3,86 +3,43 @@ const testing = std.testing;
 const mem = std.mem;
 const fs = std.fs;
 const debug = std.debug;
+const SwfReader = @import("swf_reader.zig").SwfReader;
 
 pub const SwfFile = struct {
+    /// 32-bit 16.16 fixed-point number.
+    pub const Fixed = u32;
+    /// 16-bit 8.8 fixed-point number.
+    pub const Fixed8 = f16;
+
     pub const Rect = struct {
-        bytes: []u8,
-
-        pub fn nBits(self: Rect) u5 {
-            return self.bytes[0];
-        }
-
-        pub fn xMin(self: Rect) i32 {
-            return 0;
-        }
+        xMin: i16,
+        xMax: i16,
+        yMin: i16,
+        yMax: i16,
     };
 
     pub const Header = struct {
         signature: [3]u8,
+        /// Single byte SWF version.
         version: u8,
+        /// Length of entire file in bytes.
         file_len: u32,
-        frame_size: Rect
+        /// Frame size in twips.
+        frame_size: Rect,
+        /// Frame delay in 8.8 fixed number of frames per second.
+        frame_rate: Fixed8,
+        /// Total number of frames in file.
+        frame_count: u16,
     };
 
-    header: *Header,
+    header: Header,
 
-    pub fn read(data: []u8) SwfFile {
-        return SwfReader.init(data).swf;
+    pub fn init(stream: anytype) !SwfFile {
+        var reader = SwfReader(@TypeOf(stream)).init(stream);
+        return SwfFile{
+            .header = try reader.readHeader(),
+            
+        };
     }
+
 };
-
-test "size" {
-    testing.expect(8 == @sizeOf(SwfFile.Header));
-}
-
-pub const SwfReader = struct {
-    const Self = @This();
-
-    buf: []u8,
-    pos: u64,
-    swf: SwfFile,
-
-    pub fn init(data: []u8) SwfReader {
-        var r: SwfReader = undefined;
-        r.buf = data;
-        r.pos = 0;
-
-        // r.swf.header = r.readRaw(SwfFile.Header);
-
-        r.swf.header.frame_size = r.readRect();
-        return r;
-    }
-
-    // fn readRaw(self: *Self, comptime T: type) *T {
-    //     defer self.pos += @sizeOf(T);
-    //     return @ptrCast(*T, self.buf[self.pos..@sizeOf(T)]);
-    // }
-
-    fn readRect(self: *Self) SwfFile.Rect {
-        const n_bits = @intCast(usize, self.buf[self.pos]>>3);
-        const n_bytes = ((5 + 4*n_bits) + 7) / 8;
-
-        debug.print("n_bits={} and n_bytes={}\n",.{n_bits, n_bytes});
-        defer self.pos += n_bytes;
-        return SwfFile.Rect{.bytes = self.buf[self.pos..self.pos+n_bytes]};
-    }
-
-    fn readU8(self: *Self) u8 {
-        defer self.pos += 1;
-        return self.buf[self.pos..self.pos+1];
-    }
-
-    fn read(self: *Self, comptime T: type) T {
-        defer self.pos += @sizeOf(T);
-        return self.buf[self.pos..self.pos+@sizeOf(T)];
-    }
-};
-
-test "load file" {
-    // debug.print("TEST INFO BLAHBLAH", .{});
-    // debug.print("{}", .{fs.cwd()});
-    // const allocator = std.testing.allocator;
-    // const data = try fs.cwd().readFileAlloc(allocator, "../../assets/platform-racing-2-v159.swf", 0xffffff);
-    // const swf = SwfFile.read(data);
-    // debug.print("{}", .{swf.header});
-}
